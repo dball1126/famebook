8aa95dd971377b704c764c2f1e270764fdde3e62
daniel <g.thirteen333@gmail.com>
2019-10-22 19:38:06 -0400

    homepage container and component added

---
+++ frontend/homepage/homepage.jsx
@@ -0,0 +1,15 @@
+class Homepage extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div>
+                "Hello World"
+            </div>
+        )
+    }
+}
+
+export default Homepage;
---
+++ frontend/homepage/homepage_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import Homepage from './homepage';
+
+const msp = (state) => {
+    return {
+
+    }
+}
+
+const mdp = (dispatch) => {
+    return {
+
+    }
+}
+
+export default connect(msp, mdp)(Homepage);
