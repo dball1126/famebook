0b9fb56e0b790c369b1908adb8c85f1e5eaf11b1
daniel <g.thirteen333@gmail.com>
2019-10-22 18:31:36 -0400

    user model complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ApplicationRecord
-    # validates :email, :password_digest, :session_token
+    validates :email, :password_digest, :session_token, presence: true
+    validates :password, length: {minimum: 8}, allow_nil: true
+
+    def is_password?(password)
+        new_password = BCrypt::Password.new(self.password_digest)
+        new_password.is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
+    def self.generate_session_token
+        SecureRandom.url_safe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+    end
 end
