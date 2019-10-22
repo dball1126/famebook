5489cf4d84a0f2f0e6f32c754f4156367a1865fd
daniel <g.thirteen333@gmail.com>
2019-10-22 19:24:30 -0400

    user created successfully

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,18 @@ class User < ApplicationRecord
     validates :email, :password_digest, :session_token, presence: true
     validates :password, length: {minimum: 8}, allow_nil: true
 
+    attr_reader :password
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
+
     def is_password?(password)
         new_password = BCrypt::Password.new(self.password_digest)
         new_password.is_password?(password)
@@ -17,7 +29,7 @@ class User < ApplicationRecord
     end
 
     def self.generate_session_token
-        SecureRandom.url_safe_base64
+        SecureRandom.urlsafe_base64
     end
 
     def reset_session_token!
