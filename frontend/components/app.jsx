bb88cb38f6f6ef56ed1a21c6994c0de260b278b9
daniel <g.thirteen333@gmail.com>
2019-10-22 21:56:58 -0400

    session controller created

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import HomepageContainer from './homepage/homepage_container';
 import { Route } from 'react-router-dom';
 import SignupContainer from './session/signup_container';
+import LoginContainer from './session/login_container';
+
 const App = () => {
     return (
         <div>
             <Route exact path='/' component={HomepageContainer}/>
             <Route path='/signup' component={SignupContainer}/>
+            <Route path='/login' component={LoginContainer}/>
+
         </div>
     )
 }
