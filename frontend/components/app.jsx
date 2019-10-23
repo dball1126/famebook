a73ac08aa78a7462fad13c5839adc153c0af2994
daniel <g.thirteen333@gmail.com>
2019-10-22 20:03:13 -0400

    root reducer shell complete

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,1 +1,13 @@
 import React from 'react';
+import HomepageContainer from './homepage/homepage_container';
+import { Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div>
+            <Route exact path='/' component={HomepageContainer}/>
+        </div>
+    )
+}
+
+export default App;
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,3 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
+import configureStore from './store/store'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id}, 
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser}
+            }
+        };
+        store = configureStore(preloadedState);
+    } else {
+        store = configureStore();
+    
+    }
+    ReactDOM.render(<Root store={store}/>, root);
+});
