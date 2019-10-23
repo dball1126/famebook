0572fc5dddef6a8d022d4632c1eef72d77643d12
daniel <g.thirteen333@gmail.com>
2019-10-22 20:36:39 -0400

    session reducer complete

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,18 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+
+const nullSession = Object.freeze({id: null});
+
+const sessionReducer = (oldState = nullSession, action) => {
+    Object.freeze(oldState);
+
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return {id: action.user.id}
+        case LOGOUT_CURRENT_USER:
+            return nullSession;
+        default:
+            return oldState;
+    }
+}
+
+export default sessionReducer;
