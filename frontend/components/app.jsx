04a286b295e92dc0262ba3d4c072416679176c3f
daniel <g.thirteen333@gmail.com>
2019-10-22 21:10:34 -0400

    hello world renders successfully

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import HomepageContainer from './homepage/homepage_container';
 import { Route } from 'react-router-dom';
-
+import SignupContainer from './session/signup_container';
 const App = () => {
     return (
         <div>
             <Route exact path='/' component={HomepageContainer}/>
+            <Route path='/signup' component={SignupContainer}/>
         </div>
     )
 }
--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 class Homepage extends React.Component {
     constructor(props){
         super(props)
