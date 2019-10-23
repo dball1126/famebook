b94317bf1db4165e1f3ab7e8d6016f8c26712102
daniel <g.thirteen333@gmail.com>
2019-10-22 23:58:25 -0400

    user and post associations complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,4 +15,10 @@
 
 class Post < ApplicationRecord
     validates :author_id, presence: true
+    
+    belongs_to :user,
+    primary_key: :id, 
+    foreign_key: :author_id,
+    class_name: :User
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,11 @@ class User < ApplicationRecord
 
     attr_reader :password
     after_initialize :ensure_session_token
+    
+    has_many :posts,
+        primary_key: :id, 
+        foreign_key: :author_id,
+        class_name: :Post
 
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
