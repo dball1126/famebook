3e526d47e25b7d48b97b47152ef06c0c0560bbd4
daniel <g.thirteen333@gmail.com>
2019-10-22 23:09:13 -0400

    user auth complete

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,8 +4,8 @@ import { Route } from 'react-router-dom';
 import SignupContainer from './session/signup_container';
 import LoginContainer from './session/login_container';
 import NavBarContainer from './navbar/navbar_container';
-import {AuthRoute} from '../util/route_utils';
-
+import { AuthRoute, ProtectedRoute } from '../util/route_utils';
+// <ProtectedRoute path='/posts' />  for the bottom
 const App = () => {
     return (
         <div>
--- frontend/util/route_utils.jsx
+++ frontend/util/route_utils.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Redirect, Route, withRouter } from 'react-router-dom';
 
 const msp = state => {
-    debugger
+    
     let userId = state.session.id || [];
     let currentUser;
     if (userId) currentUser = state.entities.users[userId];
@@ -20,4 +20,13 @@ const Auth = ({ loggedIn, path, component: Component }) => (
                />    
 );
 
+const Protected = ({ loggedIn, path, component: Component}) => (
+    <Route path={path}
+           render={props => (
+               loggedIn ? <Component {...props} /> : <Redirect to="signup" />
+           )}
+        />
+);
+
 export const AuthRoute = withRouter(connect(msp)(Auth))
+export const ProtectedRoute = withRouter(connect(msp)(Protected))
