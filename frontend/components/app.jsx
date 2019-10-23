99bf3ab38684a432f8730211e0e236ee27d07655
daniel <g.thirteen333@gmail.com>
2019-10-22 22:15:43 -0400

    navbar container added and rendering

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,10 +3,12 @@ import HomepageContainer from './homepage/homepage_container';
 import { Route } from 'react-router-dom';
 import SignupContainer from './session/signup_container';
 import LoginContainer from './session/login_container';
+import NavBarContainer from './navbar/navbar_container';
 
 const App = () => {
     return (
         <div>
+            <Route path ="/" component={NavBarContainer}/>
             <Route exact path='/' component={HomepageContainer}/>
             <Route path='/signup' component={SignupContainer}/>
             <Route path='/login' component={LoginContainer}/>
