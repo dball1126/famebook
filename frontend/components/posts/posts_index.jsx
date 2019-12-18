9a6b66162ec701782992d18c56f465bebfce2912
daniel <g.thirteen333@gmail.com>
2019-12-18 18:42:58 -0500

    posts index now ready for passing posts into posts index item

--- frontend/components/posts/posts_index.jsx
+++ frontend/components/posts/posts_index.jsx
@@ -4,13 +4,33 @@ import {withRouter}from 'react-router-dom';
 class Posts extends React.Component {
     constructor(props){
         super(props);
+        this.state = ({
+            posts: props.posts
+        })
     }
 
     componentDidMount(){
-        this.props.fetchUserPosts(this.props.userId);
+        this.props.fetchUserPosts(this.props.userId).then(() => this.setState({posts: this.props.posts}));
+        
+    }
+
+    userPosts(){
+       
+      let posts = this.state.posts;
+      
+       if (posts.length){
+           return (
+               <div>
+                   {"USER POSTS"}
+               </div>
+           )
+       } else {
+           return "";
+       }
     }
 
     render(){
+
         return  (
             <div className="posts-wrapper">
                 <div className="posts-box">
@@ -20,7 +40,7 @@ class Posts extends React.Component {
                     <div className="posts-right-wrapper">
                         <div className="posts-right-inner-left-box">
                             <h1>posts right inner left</h1>
-
+                                {this.userPosts()}
                         </div>
                         <div className="posts-right-inner-right-box">
                             <h1>posts right inner right</h1>
