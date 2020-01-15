c9cdaaf884cc28c0a6c1e38c33d2a3d87360819a
daniel <g.thirteen333@gmail.com>
2020-01-14 23:02:43 -0500

    search bar container and component shells complete

---
+++ frontend/components/search/search_bar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SearchBar {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <>
+
+            </>
+        )
+    }
+}
+export default SearchBar;
---
+++ frontend/components/search/search_container.js
@@ -0,0 +1,11 @@
+import { connect } from "react-redux"
+import SearchBar from './search_bar';
+const msp = (props) => {
+
+}
+
+const mdp = (prop) => {
+
+}
+
+export default connect(msp, mdp)(SearchBar);
