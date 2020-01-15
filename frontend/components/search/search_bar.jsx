f4bd9aaee62b1ab90febcf6664a3fc282883741b
daniel <g.thirteen333@gmail.com>
2020-01-15 00:58:47 -0500

    functions added to search component and resolved a bundler error where it would not show on the dom

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -1,8 +1,60 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 class SearchBar extends React.Component{
     constructor(props){
         super(props);
+        this.state = {query: "", results: ""}
+    }
+
+    handleSubmit(e){
+        e.preventDefault(); // Used to prevent the page from refreshing
+        this.props.searchUsers(this.state)
+    }
+
+    update(field){
+        return (e) => {
+            this.setState({[field]: e.target.value}, () => this.searchData())
+        }
+    }
+
+    searchData(){
+        let allData = "";
+
+        if (this.state.query.length >= 1) {
+            allData = this.props.searchUsers(this.state).then((data) => {
+                let searchUsers;
+                if (data.users){
+                    searchUsers = Object.values(data.users)
+                } else {
+                    searchUsers = "";
+                }
+                this.setState({results: searchUsers})
+
+                return searchUsers
+            })
+            return allData;
+        } else {
+
+            this.setState({results: ""})
+        }
+    }
+
+    searchResults(){
+        if (this.state.results.length < 1) {
+            return ""
+        } else {
+            let users = this.state.results;
+            const allUsers = users.map((user, i) => {
+                return (
+                    <div key={i}>
+                        <h1>{user.username}</h1>
+                    </div>
+                )
+            })
+
+            return allUsers;
+        }
     }
 
     render(){
@@ -10,13 +62,22 @@ class SearchBar extends React.Component{
             <>
                 <div className="search-bar-wrapper">
                     <div className="search-bar-box">
-                        <form action="">
-                            <input placeholder="Search" className="search-input"/>
+                        <form action="" onSubmit={this.handleSubmit}>
+                            <input placeholder="Search" 
+                                   className="search-input"
+                                   onChange={this.update('query')}
+                                   type="text"/>
                         </form>
+
+                        <div className="search-data">
+
+                            {this.searchResults()}
+
+                        </div>
                     </div>
                 </div>
             </>
         )
     }
 }
-export default SearchBar;
+export default withRouter(SearchBar);
--- frontend/components/search/search_container.js
+++ frontend/components/search/search_container.js
@@ -1,12 +1,18 @@
 import { connect } from "react-redux"
 import SearchBar from './search_bar';
+import {searchUsers} from '../../actions/search_actions';
 
 const msp = (props) => {
+    
+    return {
 
+    }
 }
 
-const mdp = (prop) => {
-
+const mdp = (dispatch) => {
+    return {
+        searchUsers: users => dispatch(searchUsers(users))
+    }
 }
 
 export default connect(msp, mdp)(SearchBar);
