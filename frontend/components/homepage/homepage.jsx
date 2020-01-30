7aefb11b9c3dccb38a3221a06d15a0c2e9106e64
Daniel Ball <g.thirteen333@gmail.com>
2020-01-29 19:03:49 -0500

    update to background for splash screen and left side content added as well

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -12,7 +12,24 @@ class Homepage extends React.Component {
             <div className="homepage-wrapper">
                 <div className="homepage-box">
                     <div className="homepage-box-left">
-                            <h1>BOX LEFT</h1>
+                    <div className="homepage-left-heading">
+                                <h2>Connect with friends and the world around you on Facebook.</h2>
+                            </div>
+
+                            <div className="homepage-left-content">
+                                <img src="https://i.imgur.com/xLAAbWo.png"/>
+                                <span>See photos and updates</span> from friends in News Feed.
+                            </div>
+
+                            <div className="homepage-left-content">
+                                <img src="https://i.imgur.com/hL8776j.png"/>
+                                <span>Share what's new</span> in your life on your Timeline.
+                            </div>
+
+                            <div className="homepage-left-content">
+                                <img src="https://i.imgur.com/Si6EwCM.png"/>
+                                <span>Find more</span> of what you're looking for with Facebook Search.
+                            </div>
                     </div>
                     <div className="homepage-box-right">
 
